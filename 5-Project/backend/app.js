d003e9c0571ee7e2604d1f5210c672adbd4c4a6f
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-08-08 11:20:54 +0530

    Project

--- 5-Project/backend/app.js
+++ 5-Project/backend/app.js
@@ -1,11 +1,13 @@
 // External Module
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/mongodb.config.js'
 
 const app = express()
 
 dotenv.config()
 const PORT = process.env.PORT || 3000;
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Server Start')
