b2caa08ee4a8c7c0fb9213e67a40b5d88f0eabda
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-08-12 17:25:35 +0530

    Project

--- 5-Project/backend/routes/user.router.js
+++ 5-Project/backend/routes/user.router.js
@@ -1,23 +1,46 @@
-import express from 'express'
-import { refereshAccessToken, userLogin, userLogout, userRegistration } from '../controllers/user.controllers.js'
-import { upload } from '../middleware/multer.middleware.js'
-import { userAuth } from '../middleware/auth.middleware.js'
+import express from "express";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  refereshAccessToken,
+  updateAccountDetails,
+  updateCoverImage,
+  updateUserAvtar,
+  userLogin,
+  userLogout,
+  userRegistration,
+} from "../controllers/user.controllers.js";
+import { upload } from "../middleware/multer.middleware.js";
+import { userAuth } from "../middleware/auth.middleware.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.post('/register',upload.fields([
-  {
-    name: 'avatar',
-    maxCount: 1
-  },
-  {
-    name: 'coverImage',
-    maxCount: 1
-  }
-]), userRegistration)
+userRouter.post(
+  "/register",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  userRegistration
+);
 
-userRouter.post('/login', userLogin)
-userRouter.post('/logout',userAuth, userLogout)
-userRouter.post('/refreshToken', refereshAccessToken)
+userRouter.post("/login", userLogin);
+userRouter.post("/logout", userAuth, userLogout);
+userRouter.post("/refreshToken", refereshAccessToken);
+userRouter.post("/changePassword", userAuth, changeCurrentPassword);
+userRouter.get("/currentUser", userAuth, getCurrentUser);
+userRouter.patch("/updateAccount", userAuth, updateAccountDetails);
+userRouter.patch("/avatar", userAuth, upload.single("avatar"), updateUserAvtar);
+userRouter.patch("/coverImage", userAuth,upload.single("coverImage"),updateCoverImage);
+userRouter.get("/c/:userName", userAuth, getUserChannelProfile);
+userRouter.get("/history", userAuth, getWatchHistory);
 
-export { userRouter }
+export { userRouter };
