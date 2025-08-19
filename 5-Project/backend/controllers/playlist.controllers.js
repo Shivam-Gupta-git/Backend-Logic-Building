c75171f8b5bba9ba6a5bbe0cdd0e79b5798e2d49
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-08-20 00:03:06 +0530

    Project

--- 5-Project/backend/controllers/playlist.controllers.js
+++ 5-Project/backend/controllers/playlist.controllers.js
@@ -28,7 +28,7 @@ export const createPlaylist = async (req, res) => {
   console.error("Error while creating playlist:", error)
   return res.status(500).json({success: false, message: "Error while creating playlist", error: error.message})
  }
-}
+};
 
 export const getUserPlaylist = async (req, res) => {
 try {
@@ -52,4 +52,112 @@ try {
   console.error("Error while in getUserPlaylist")
   return res.status(500).json({success: false, message: error.message})
 }
+};
+
+export const getUserPlaylistById = async (req, res) => {
+try {
+   const { playlistId } = req.params;
+   if(!playlistId?.trim()){
+    return res.status(400).json({success: false, message: "playlist Id is required"})
+   }
+   
+   const existingPlaylistId = await Playlist.findById(playlistId)
+   if(!existingPlaylistId){
+    return res.status(400).json({success: false, message: "Playlist Id not found"})
+   }
+  
+   return res.status(200).json({success: true, message: "existing playlist id fetching successfully", data: existingPlaylistId})
+} catch (error) {
+  console.error("Error while in getUserPlaylistById", error)
+  return res.status(500).json({success: false, message: error.message})
 }
+};
+
+export const addVideoInPlaylist = async (req, res) => {
+try {
+    const { videoId, playlistId } = req.params;
+  
+    if(!videoId?.trim()){
+      return res.status(400).json({success: false, message: "Video Id is required"})
+    }
+  
+    if(!playlistId?.trim()){
+      return res.status(400).json({success: false, message: "playlist Id is required"})
+    }
+  
+    const userPlaylist = await Playlist.findById(playlistId)
+    if(!userPlaylist){
+      return res.status(400).json({success: false, message: "playlist Id not found"})
+    }
+    if(userPlaylist.videos.includes(videoId)){
+      return res.status(400).json({ success: false, message: "Video already exists in this playlist" });
+    }
+  
+    userPlaylist.videos.push(videoId);
+    await userPlaylist.save()
+  
+    return res.status(200).json({success: true, message: "Video added to playlist successfully", data: userPlaylist})
+} catch (error) {
+  console.error("Error while in addVideoInPlaylist", error)
+  return res.status(500).json({success: false, message: error.message})
+}
+};
+
+export const removeVideoInPlaylist = async (req, res) => {
+ try {
+  const { videoId, playlistId } = req.params;
+  if(!videoId?.trim()){
+   return res.status(400).json({success: false, message: "video Id is required"})
+  }
+ 
+  if(!playlistId?.trim()){
+   return res.status(400).json({success: false, message: "playlist Id is required"})
+  }
+ 
+  const userPlaylist = await Playlist.findById(playlistId)
+  if(!userPlaylist){
+   return res.status(400).json({success: false, message: "playlist Id is not found"})
+  }
+ 
+  await Playlist.findByIdAndUpdate
+  (playlistId,{
+    $pull:{ videos: videoId },
+  },
+  {new: true}
+  )
+  return res.status(200).json({success: true, message: "removeVideoInPlaylist is successfully"})
+ } catch (error) {
+  console.error("Error while in removeVideoInPlaylist")
+  return res.status(500).json({success: false, message: error.message})
+ }
+};
+
+export const updatePlaylist = async (req, res) => {
+ try {
+   const { playlistId } = req.params
+   const { name, description } = req.body;
+ 
+   if(!playlistId?.trim()){
+     return res.status(400).json({success: false, message: "playlist Id is required"})
+   }
+   if([name, description].some((fields) => fields?.trim() === "")){
+     return res.status(400).json({success: false, message: "all fields are required"})
+   }
+ 
+   const existingPlaylistId = await Playlist.findById(playlistId)
+   if(!existingPlaylistId){
+     return res.status(400).json({success: false, message: "playlist Id is not found"})
+   }
+ 
+   const updatePlaylist = await Playlist.findByIdAndUpdate(existingPlaylistId, {
+     name,
+     description
+   })
+ 
+   return res.status(200).json({success: true, message: "updating playlist successfully", data: updatePlaylist})
+ } catch (error) {
+  console.error("Error while in updatePlaylist", error)
+  return res.status(500).json({success: false, message: error.message})
+ }
+};
+
--- 5-Project/backend/routes/playlist.router.js
+++ 5-Project/backend/routes/playlist.router.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { createPlaylist, getUserPlaylist } from '../controllers/playlist.controllers.js'
+import { addVideoInPlaylist, createPlaylist, getUserPlaylist, getUserPlaylistById, removeVideoInPlaylist, updatePlaylist } from '../controllers/playlist.controllers.js'
 import { userAuth } from '../middleware/auth.middleware.js'
 
 const playlistRouter = express.Router()
 
 playlistRouter.post('/createPlaylist',userAuth, createPlaylist)
 playlistRouter.get('/getUserPlaylist/:owner', userAuth, getUserPlaylist)
+playlistRouter.get('/getUserPlaylistById/:playlistId', userAuth, getUserPlaylistById)
+playlistRouter.patch('/addVideoInPlaylist/:videoId/:playlistId', userAuth, addVideoInPlaylist)
+playlistRouter.delete('/removeVideoInPlaylist/:videoId/:playlistId', userAuth, removeVideoInPlaylist)
+playlistRouter.patch('/updatePlaylist/:playlistId', userAuth, updatePlaylist)
 
 export { playlistRouter }
