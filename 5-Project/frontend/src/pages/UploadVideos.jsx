bd82270f064ad3985e93b8db121d13d4d9d12d79
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-23 18:20:10 +0530

    Project 5

--- 5-Project/frontend/src/pages/UploadVideos.jsx
+++ 5-Project/frontend/src/pages/UploadVideos.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function UploadVideos() {
   return (
-    <div className='h-[100%] w-[100%] bg-transparent z-50 flex items-center justify-center'>UploadVideos</div>
-  )
+    <div className="h-[100vh] w-[100%] flex items-center justify-center mt-[-6%]">
+      <div className="h-[80%] w-[50%] border p-5 rounded border-gray-300 shadow-sm">
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Title
+          </label>
+          <input
+            type="text"
+            className="mt-1 block px-3 py-2 border border-gray-300 w-full rounded shadow-sm resize-none focus:outline-none focus:ring-2 focus:ring-gray-400"
+            placeholder="Enter Video title"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mt-2">
+            Description
+          </label>
+         <textarea  rows="4" className="w-full mt-2 p-2 border border-gray-300 rounded shadow-sm resize-none focus:outline-none focus:ring-2 focus:ring-gray-400"></textarea>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default UploadVideos
+export default UploadVideos;
