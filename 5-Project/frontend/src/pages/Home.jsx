d9b3ed720d28f1d6e952747c362c3229ace8a68b
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-13 23:06:18 +0530

    Project

--- 5-Project/frontend/src/pages/Home.jsx
+++ 5-Project/frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChannelContext } from '../context/ChannelContext'
 
 function Home() {
+  const {token, userData, backendURL} = useContext(ChannelContext)
   return (
-    <div>Home</div>
+    <div className='w-full h-[100vh] border p-2'>
+      <div className='w-[30%] h-[300px] border rounded-2xl'></div>
+    </div>
   )
 }
 
